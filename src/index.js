95b7fbfac3113b7dedc9ee826f231b6c031d09ee
nunjachomp <nunjachomp@gmail.com>
2023-08-06 20:50:50 +0300

    Added Firebase Files

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { BytContextProvider } from "../src/Context/BytContext";
 import './index.css';
 import App from './App';
+import {initializeApp} from 'firebase/app'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA0HBtnJzshbkiZXhFrBpaTO9q_U5kz-n0",
+    authDomain: "bytterapp.firebaseapp.com",
+    projectId: "bytterapp",
+    storageBucket: "bytterapp.appspot.com",
+    messagingSenderId: "134938456952",
+    appId: "1:134938456952:web:1528ed756f8efc7f79ce8d"
+  };
 
+initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
